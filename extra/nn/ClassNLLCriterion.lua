17e47dcdcef71dd64ca3ad35596bb7ec8993c6b7
Sixin Zhang <zsx@rose7.cs.nyu.edu>
2013-04-09 11:54:35 -0400

    class nll

--- extra/nn/ClassNLLCriterion.lua
+++ extra/nn/ClassNLLCriterion.lua
@@ -6,14 +6,15 @@ function ClassNLLCriterion:__init()
 end
 
 function ClassNLLCriterion:updateOutput(input, target)
---   input.nn.ClassNLLCriterion_updateOutput(self, input, target)
---   return self.output
-
+   input.nn.ClassNLLCriterion_updateOutput(self, input, target)
+   return self.output
+--[[
    if input:dim() == 1 then
       self.output = -input[target]
    elseif input:dim() == 2 then
       local output = torch.Tensor(1):typeAs(input)
       for i=1,target:size(1) do
+         -- output:add(-1,input[{{i},target[i]}])
          output:add(-1,input[i][{{target[i]}}])
       end
       if self.sizeAverage then
@@ -24,29 +25,12 @@ function ClassNLLCriterion:updateOutput(input, target)
       error('matrix or vector expected')
    end
    return self.output
-
+--]]
 end
 
 function ClassNLLCriterion:updateGradInput(input, target)
    self.gradInput:resizeAs(input)
    self.gradInput:zero()
-
---   input.nn.ClassNLLCriterion_updateGradInput(self, input, target)
---   return self.gradInput
-
-  if input:dim() == 1 then
-      self.gradInput[target] = -1
-   else
-      local z = -1
-      if self.sizeAverage then
-         z = z / target:size(1)
-      end
-      local gradInput = self.gradInput
-      for i=1,target:size(1) do
-         gradInput[i][target[i]] = z
-      end
-   end
-
-   return self.gradInput   
-
+   input.nn.ClassNLLCriterion_updateGradInput(self, input, target)
+   return self.gradInput
 end
